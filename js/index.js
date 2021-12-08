0c461441d6ea4921147e53ad8c688758ab5d60b6
Umidjon <umidzhonofficialdev@gmail.com>
2021-12-09 00:57:25 +0600

    Loader

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-console.log();
+const loader = document.querySelector(".loader");
+const text = document.querySelector(".content");
+
+setTimeout(()=>{
+    loader.style.opacity = 0;
+    text.style.opacity = 0;
+    setTimeout(()=>{
+        loader.classList.add("hidden");
+        text.classList.add("visible")
+        text.style.opacity = 1
+    },800)
+    
+},5000)
