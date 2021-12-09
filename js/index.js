e3e20178faf9d8b976e29232d738c71e9c133fcf
Umidjon <umidzhonofficialdev@gmail.com>
2021-12-09 18:53:27 +0600

    Loader Animation

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
 const loader = document.querySelector(".loader");
-const text = document.querySelector(".content");
+const content = document.querySelector(".content");
+const text = document.querySelector(".title");
 
 setTimeout(()=>{
     loader.style.opacity = 0;
-    text.style.opacity = 0;
+    content.style.opacity = 0;
     setTimeout(()=>{
         loader.classList.add("hidden");
-        text.classList.add("visible")
-        text.style.opacity = 1
+        content.classList.add("visible")
+        content.style.opacity = 1
     },800)
+    setTimeout(()=>{
+        text.classList.add("scale")
+    },400)
     
 },5000)
